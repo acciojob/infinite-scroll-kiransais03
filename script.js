6a56de51aa8c53b186ba5228267094a5b9b64be1
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-06-27 23:21:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here!
-
+let div1=document.getElementById('infi-list');
+let ct=10;
+div1.onscroll=function (ev) {
+	// console.log(div1.scrollTop,div1.clientHeight,div1.scrollHeight)
+	if (div1.scrollTop + div1.clientHeight >= div1.scrollHeight){
+		let li1=document.createElement('li');
+    ct++;
+    li1.innerText=`Item ${ct}`;
+    let li2=document.createElement('li');
+    ct++;
+    li2.innerText=`Item ${ct}`;
+    div1.append(li1,li2);
+	}
+}
